ef507d2f0a708a7dae338bacf40e9bf3cc9b6201
nandawanda123 <208420100011@umsida.ac.id>
2023-06-22 13:11:03 +0800

    push

--- src/utils/pipes.js
+++
@@ -1,5 +0,0 @@
-export const pluralize = (count, singular, plural) => {
-  const pluralRules = new Intl.PluralRules("es-ES");
-  const numbering = pluralRules.select(count);
-  return `${count} ${numbering === "one" ? singular : plural}`;
-};
